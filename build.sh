1b3ef595a3e27fa3a3e0ab3a87f228647cbf5591
shastagrande <48636829+shastagrande@users.noreply.github.com>
2025-02-19 18:41:55 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-git clone https://github.com/PaperMC/Folia
+git clone -b dev/hard-fork --single-branch https://github.com/PaperMC/Folia
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
 cd Folia
-./gradlew applyPatches
-./gradlew createReobfPaperclipJar
+git submodule update --init --recursive
+./gradlew applyAllPatches
+./gradlew createMojmapPaperclipJar
